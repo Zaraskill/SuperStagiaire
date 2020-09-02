41c5199e64b241abd96f0f5de85cf18c53457487
FrancescoPanini <35837701+FrancescoPanini@users.noreply.github.com>
2020-09-02 15:05:02 +0200

    Camera movement
    Lerp from room to room when press space (will be replaced by a trigger)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 officeTrans;
+    public Vector3 sideroomTrans;
+    public Vector3 offset;
+    public float cameraSpeed;
+
+    private bool isInOffice;
+    private Vector3 destination;
+    private float destinationSensitivity;
+
+    private bool isMoving;
+
+
     void Start()
     {
-        
+        this.transform.position = officeTrans + offset;
+        isInOffice = true;
+
+        destinationSensitivity = 0.05f;
+        isMoving = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    // When space is pressed, cameraview changes from one room to the other
+    void LateUpdate()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            destination = isInOffice ? sideroomTrans + offset : officeTrans + offset;
+            isInOffice = !isInOffice;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            if (this.transform.position.x < destination.x - destinationSensitivity  || this.transform.position.x > destination.x + destinationSensitivity)
+                this.transform.position = Vector3.Lerp(this.transform.position, destination, cameraSpeed * Time.deltaTime);
+            else
+            {
+                this.transform.position = destination;
+                isMoving = false;
+            }
+        }
     }
 }
